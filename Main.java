05e5b86ece8dd381f18ee91d07fead219a7dc9c7
goatess <107187023+goatess@users.noreply.github.com>
2022-06-09 16:40:25 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+   public static void main(String[] args) {
+       int numeroIf = 0;
+       int numeroWhile = 0;
+       int numeroFor = 0;
+
+       if (numeroIf > 0) {
+           System.out.println(numeroIf);
+       } else if (numeroIf<0){
+           System.out.println(numeroIf);
+       } else {
+           System.out.println(numeroIf);
+       }
+
+       while (numeroWhile < 3) {
+           numeroWhile = numeroWhile + 1;
+           System.out.println(numeroWhile);
+       }
+
+       do {
+        numeroWhile = numeroWhile + 1;
+        
+       } while (numeroWhile < 3); 
+        System.out.println(numeroWhile);
+
+       for
+
+       switch (key) {
+           case value:
+               
+               break;
+       
+           default:
+               break;
+       }
+           
+   }
+
+
+}
